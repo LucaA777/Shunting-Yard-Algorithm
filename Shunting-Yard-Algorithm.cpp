9b49d984af8ea5a61708c269835f0bb48840e193
U-BSD\497022 <497022@SHS-8YBF6X3.beaverton.k12.or.us>
2026-01-28 09:42:55 -0800

    Fixed input so that input processing works.

--- Shunting-Yard-Algorithm.cpp
+++ Shunting-Yard-Algorithm.cpp
@@ -1,6 +1,6 @@
 /*
-Implementation of the shunting yard algorithm using stacks and queues implemented
-with linked lists.
+   Implementation of the shunting yard algorithm using stacks and queues implemented
+   with linked lists.
 
 Author: Luca Ardanaz
 Last Updated: 27/1/2026
@@ -19,70 +19,79 @@ Queue<string>* createInfixFromString(string input);
 
 int main() {
 
-  //basic tester to see if the infix creation works
+	//basic tester to see if the infix creation works
 
-  cout << "Enter an expression (space seperated): " << endl;
-  string input;
-  cin >> input;
+	cout << "Enter an expression (space seperated): " << endl;
+	string input;
+	getline(cin, input);
 
-  Queue<string>* infix = createInfixFromString(input);
-  cout << "Is empty?: " << infix -> isEmpty();
-  string out;
-  while (!infix -> isEmpty()) {
-    cout << infix -> dequeue() << " ";
-  }
-  cout << endl;
+	Queue<string>* infix = createInfixFromString(input);
+	string out;
+	while (!infix -> isEmpty()) {
+		cout << infix -> dequeue() << " ";
+	}
+	cout << endl;
 
-  return 0;
+	return 0;
 }
 
 Queue<string>* createInfixFromString(string input) {
-  Queue<string>* output = new Queue<string>();
-  string substring = "";
-  for (char c : input) {
-    
-    //if a space is reached, check if the substring is valid.
-    if (c == ' ') {
-      
-      //if it is valid, add to output
-      if (validString(substring)) {
-        output -> enqueue(substring);
-        substring = "";
-      }
-      //otherwise, abort.
-      else {
-        return new Queue<string>();
-      }
-    }
+	Queue<string>* output = new Queue<string>();
+	string substring = "";
+	for (char c : input) {
 
-    //if not a space, add it to the substring
-    else {
-      substring += c;
-    }
-  }
+		//if a space is reached, check if the substring is valid.
+		if (c == ' ') {
 
-  return output;
+			//if it is valid, add to output
+			if (validString(substring)) {
+				output -> enqueue(substring);
+				substring = "";
+			}
+			//otherwise, abort.
+			else {
+				return new Queue<string>();
+			}
+		}
+
+		//if not a space, add it to the substring
+		else {
+			substring += c;
+		}
+	}
+
+	//add last substring to the output if its valid
+	if (validString(substring)) {
+		output -> enqueue(substring);
+		substring = "";
+	}
+	else {
+		return new Queue<string>();
+	}
+
+
+	return output;
 }
 
 bool validString(string input) {
-  //check if the string is a number, if so it is an operand and thus valid.
-  try {
-    double num = stoi(input);
-    return true;
-  }
-  catch(...) {}
+	//check if the string is a number, if so it is an operand and thus valid.
+	try {
+		double num = stoi(input);
+		return true;
+	}
+	catch(...) {}
 
-  //check if the string is in the valid set of operators.
-  if (input == "+" ||
-      input == "-" ||
-      input == "*" ||
-      input == "/" ||
-      input == "(" ||
-      input == ")") 
-  {
-    return true;  
-  }
-  cout << "INVALID INPUT" << endl;
-  return false;
+	//check if the string is in the valid set of operators.
+	if (input == "+" ||
+			input == "-" ||
+			input == "*" ||
+			input == "/" ||
+			input == "(" ||
+			input == ")") 
+	{
+		return true;  
+	}
+	cout << "INVALID INPUT" << endl;
+	return false;
 }
 
