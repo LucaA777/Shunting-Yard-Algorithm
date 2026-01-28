ac30e73579d0fb4ed8536591552f337228d229cb
U-BSD\497022 <497022@SHS-8YBF6X3.beaverton.k12.or.us>
2026-01-28 10:22:17 -0800

    Attempted to translate infix to postfix

--- Shunting-Yard-Algorithm.cpp
+++ Shunting-Yard-Algorithm.cpp
@@ -11,11 +11,14 @@ Last Updated: 27/1/2026
 
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
 bool validString(string input);
 Queue<string>* createInfixFromString(string input);
+Queue<string>* infixToPostfix(Queue<string>* infix);
+bool shouldPopStack(string newOperator, string stackOperator);
 
 int main() {
 
@@ -26,9 +29,17 @@ int main() {
 	getline(cin, input);
 
 	Queue<string>* infix = createInfixFromString(input);
+
+	Queue<string>* postfix = infixToPostfix(infix);
+
 	string out;
 	while (!infix -> isEmpty()) {
 		cout << infix -> dequeue() << " ";
+	}
+	cout << endl;
+
+	while (!postfix -> isEmpty()) {
+		cout << postfix -> dequeue() << " ";
 	}
 	cout << endl;
 
@@ -76,7 +87,7 @@ Queue<string>* createInfixFromString(string input) {
 bool validString(string input) {
 	//check if the string is a number, if so it is an operand and thus valid.
 	try {
-		double num = stoi(input);
+		double num = stod(input);
 		return true;
 	}
 	catch(...) {}
@@ -87,7 +98,8 @@ bool validString(string input) {
 			input == "*" ||
 			input == "/" ||
 			input == "(" ||
-			input == ")") 
+			input == ")" ||
+			input == "^") 
 	{
 		return true;  
 	}
@@ -95,3 +107,79 @@ bool validString(string input) {
 	return false;
 }
 
+Queue<string>* infixToPostfix(Queue<string>* infix) {
+	Queue<string>* output = new Queue<string>();
+	Stack<string>* stack = new Stack<string>();
+
+	//go through the infix and process the strings
+	while (!infix -> isEmpty()) {
+		string element = infix -> dequeue();
+
+		//if the element is a number, push it to the output
+		try {
+			double num = stod(element);
+			output -> enqueue(element);
+			continue;
+		}
+		catch (...) {}
+
+		//if the element is an operator, process it
+		
+		//always push open parenthesis
+		if (element == "(") {
+			stack -> push(element);
+		}
+
+		//when closing parenthesis, pop until matching open
+		else if (element == ")") {
+			string stackOperator;
+			do {
+				stackOperator = stack -> pop();
+
+				if (stackOperator != "(") {
+					output -> enqueue(stackOperator);
+				}
+
+			} while (stackOperator != "(");
+		}
+
+		//for other operators, check if the stack should be popped
+		//pop until it reaches a point it doesn't need to
+		else {
+			while (shouldPopStack(element, stack -> peek())) {
+				output -> enqueue(stack -> pop());
+			}
+			stack -> push(element);
+		}
+
+	}
+
+	return output;
+
+}
+
+bool shouldPopStack(string newOperator, string stackOperator) {
+	//define precedence for all the operators
+	map<string, int> precedence = {
+		{"^", 4},
+		{"*", 3},
+		{"/", 3},
+		{"+", 2},
+		{"-", 2}
+	};	
+	
+	//if new has higher precedence, it should be added to the stack
+	if (precedence[newOperator] > precedence[stackOperator]) {
+		return false;
+	}
+
+	//since ^ is right-evaluated, don't pop consecutive
+	if (newOperator == "^" && stackOperator == "^") {
+		return false;
+	}
+
+	//otherwise, the stack should be popped
+	return true;
+
+
+}
