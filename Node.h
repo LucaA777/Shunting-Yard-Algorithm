af87195b93592428fa2aba63d9972fd75a277ce9
Luca Ardanaz <luca.ardanaz@gmail.com>
2026-01-28 17:20:03 -0800

    Added a print tree function

--- Node.h
+++ Node.h
@@ -6,6 +6,8 @@ Author: Luca Ardanaz
 Last Updated: 26/1/2026
 */
 
+#include <iostream>
+
 #ifndef NODE_H
 #define NODE_H 
 
@@ -62,11 +64,36 @@ public:
     return next;
   }
 
+  void printTree() {
+    printTreeRecursion(this, 1);
+  }
+
 private:
   T value;
   Node<T>* left;
   Node<T>* right;
   Node<T>* next;
+
+  void printTreeRecursion(Node<T>* node, int depth) {
+
+    //prints everything to the right of an index first
+    if (node -> getRight() != nullptr) {
+      printTreeRecursion(node -> getRight(), depth + 1);
+    }
+
+    //adds appropriate spacing
+    for (int i = 0; i < depth - 1; i++) {
+      std::cout << "\t";
+    }	
+
+    //prints index
+    std::cout << node -> getValue() << std::endl;
+
+    //then prints everything to the left
+    if (node -> getLeft() != nullptr) {
+      printTreeRecursion(node -> getLeft(), depth + 1);
+    }
+  }
 };
 
 #endif
